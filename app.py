7775d9392b3e8dae03a853fec44780bbc523b312
lion326 <kabushiki0326@yahoo.co.jp>
2022-02-05 15:46:59 +0900

    lion-commit

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-from cgitb import handler
-from flask import Flask,request,abort
-from linebot import LineBotApi,WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent,TextMessage,TextSendMessage
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 import os
 
 app = Flask(__name__)
 
 #環境変数取得
-CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(CHANNEL_SECRET)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/callback",methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
-    #get X-Line-Signature header value
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    #get request body as text
+    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body:" + body)
+    app.logger.info("Request body: " + body)
 
-    #handle webhook body
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -31,14 +37,25 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    returnmessage = event.message.text
+    if event.message.text == "こんにちは":
+        returnmessage = "どうもこんにちは"
+    elif event.message.text == "こんばんは":
+        returnmessage = "どうもこんばんは"
+    elif event.message.text == "疲れた":
+        returnmessage = "お疲れ様でした"
+    elif event.message.text == "ネオテック":
+        returnmessage = "赤字は悪"
+    # 返信
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=returnmessage))
+
 
 if __name__ == "__main__":
-    #app,run()
-    port = int(os.getenv("PORT",5000))
-    app.run(host="0,0,0,0", port=port)
-        
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
