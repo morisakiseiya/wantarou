00960fccc022b23051bb51eb99647ea1a5ae5869
morisaki <morisaki849@gmail.com>
2022-02-05 16:59:18 +0900

    commit

--- app.py
+++ app.py
@@ -51,8 +51,9 @@ def handle_message(event):
         returnmessage = "無能"
     elif event.message.text == "今日は何日？": 
         date = datetime.date.today()
-        returnDay = date.year + "年" + date.month + "月" + date.day + "日"
-        returnmessage = returnDay
+        # returnDay = date.year + "年" + date.month + "月" + date.day + "日"
+        
+        returnmessage = date.year
     else:
         returnmessage = "ワン！"
     # 返信
