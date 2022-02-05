4ddec2db83bb0c1ad9d6602a4e20ec97b4945a93
morisaki <morisaki849@gmail.com>
2022-02-05 14:41:23 +0900

    commi3

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import os
 app = Flask(__name__)
 
 #環境変数取得
-CHANNEL_ACCESS_TOKEN = os.environ["Wx/hKbTLBMB0OyOnn0ywZA3OZcSDGfNmMqgjAaCvtNCvEeNvFRVPFB5HJmKccUASEf+iwAt+1AemtADcooLSW2HL6+kLZefLz+ULwMblOoZ1YsBJoJsTDpSNSjjtBLQQnwp7Lgl7Qdy7fcQwfQujogdB04t89/1O/w1cDnyilFU="]
-CHANNEL_SECRET = os.environ["a05d3c2571855c280b89c81e73d67379"]
+CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
